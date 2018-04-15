46fb8d1c2c377c70c25758c846101910df63ef7d
Vicki Hamm <vicki.cello@gmail.com>
2018-04-15 23:18:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 function addToCart(item) {
  var i = {itemName:item, itemPrice:Math.floor(Math.random() * 100)};
  cart.push(i);
- 
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
